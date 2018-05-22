784abac3727d41976fde27ac51b47e9a028741fe
Max Yeamans <max.yeamans@gmail.com>
2018-05-22 05:54:35 -0700

    Rearranged the code to make the game more function-based. Aside from making the page look prettier, I'd say this is done.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
-$(document).ready( function() {
-    var arrCrystalImages = ["assets/images/crystal-1.png","assets/images/crystal-2.png","assets/images/crystal-3.png","assets/images/crystal-4.png"];
+$(document).ready(function () {
+
+    var arrCrystalImages = ["assets/images/crystal-1.png", "assets/images/crystal-2.png", "assets/images/crystal-3.png", "assets/images/crystal-4.png"];
     var numRandomNumber;
     var numCurrentTotal = 0;
     var numWins = 0;
@@ -8,35 +9,53 @@ $(document).ready( function() {
     // Add crystals to the page
     for (var i = 0; i < arrCrystalImages.length; i++) {
         var imgCrystal = $("<img>");
-        var numCrystalValue = Math.floor(Math.random() * 12) + 1;
         imgCrystal.addClass("crystal-image px-2");
         imgCrystal.attr({
-            "src" : arrCrystalImages[i],
-            "value" : numCrystalValue
+            "src": arrCrystalImages[i],
+            "id": "crystal-" + i
         });
         $("#crystals-section").append(imgCrystal);
-        console.log("Crystal ", (i + 1), "value: ", numCrystalValue);
     };
 
-    // Set the game's random number
-    numRandomNumber = Math.floor(Math.random() * 101) + 19;
-    $("#random-number").text(numRandomNumber);
-    console.log("This game's random number: ", numRandomNumber);
+    InitializeGame();
+
+    function InitializeGame() {
+        // Set the game's random number between 19 and 120
+        numRandomNumber = Math.floor(Math.random() * 101) + 19;
+        $("#random-number").text(numRandomNumber);
+        console.log("This game's random number: ", numRandomNumber);
+
+        // Set the crystals' random numbers between 1 and 12
+        $(".crystal-image").each(function (index) {
+            $(this).attr("value", Math.floor(Math.random() * 12) + 1);
+            console.log("Crystal " + (index + 1) + " value:", $(this).attr("value"));
+        });
+
+        // Set the current total to 0
+        numCurrentTotal = 0;
+        $("#number-total").text(numCurrentTotal);
+    };
 
     // On crystal click, add the crystal's value to running total
-    $(".crystal-image").on("click", function() {
-        console.log( $(this).attr("value") );
+    $(".crystal-image").on("click", function () {
+        console.log($(this).attr("value"));
         numCurrentTotal += parseInt($(this).attr("value"));
         $("#number-total").text(numCurrentTotal);
-        if( numCurrentTotal > numRandomNumber) {
+        CheckForWin();
+    });
+
+    function CheckForWin() {
+        if (numCurrentTotal > numRandomNumber) {
             alert("Too high. You LOSE!");
             numLosses++;
             $("#losses").text(numLosses);
+            InitializeGame();
         }
         else if (numCurrentTotal === numRandomNumber) {
             alert("By Poseidon's beard, you did it!");
             numWins++;
             $("#wins").text(numWins);
+            InitializeGame();
         }
-    });
+    }
 });
